4591b37a95a21a432e3c75c5e68c741e8d043a44
Sameer <sameersattar1111@gmail.com>
2024-02-04 00:42:40 +0500

    express

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,17 @@
-console.log("Sameer Sattar")
+import express from "express"
+
+const app = express();
+
+app.get("/"  , (req,res) => {
+    res.status(200).send("Hello World")
+})
+
+app.get("/register"  , (req,res) => {
+    res.status(200).send("Register Yourself")
+})
+
+const PORT = 4000
+
+app.listen(PORT , () => {
+    console.log(`Server is running on PORT ${PORT}`)
+})
