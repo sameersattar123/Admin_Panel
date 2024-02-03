68a0d5189fd2eeb0f4d78f2e42e129cc4ed8aacb
Sameer <sameersattar1111@gmail.com>
2024-02-04 03:44:25 +0500

    pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import {BrowserRouter  , Route , Routes} from "react-router-dom"
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Services from './Pages/Services'
+import Register from './Pages/Register'
+import Login from './Pages/Login'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact/>} />
+      <Route path='/services'  element={<Services/>}/>
+      <Route path='/register' element={<Register/>} />
+      <Route path='/login' element={<Login/>} />
+    </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
