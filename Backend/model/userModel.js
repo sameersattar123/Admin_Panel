039408acd838b40485e4effe52ce2bbe91354d31
Sameer <sameersattar1111@gmail.com>
2024-02-04 04:18:41 +0500

    user model

--- Backend/model/userModel.js
+++ Backend/model/userModel.js
@@ -1,1 +1,31 @@
-console.log("sameer sattar")
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("User", UserSchema);
