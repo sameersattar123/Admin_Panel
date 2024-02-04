e827e8541ae76fcae0c8139d487b0033a664c5d2
Sameer <sameersattar1111@gmail.com>
2024-02-05 03:34:27 +0500

    404 page

--- Frontend/src/Pages/Error.jsx
+++ Frontend/src/Pages/Error.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Error = () => {
   return (
-    <div>Error</div>
-  )
-}
+    <section id="error-page">
+      <div className="content">
+        <h2 className="header">404</h2>
+        <h4>Sorry! Page not found</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum ad, in
+          tempora distinctio mollitia dolorum odit reiciendis laborum ducimus
+          quod, veniam ipsa, asperiores iste. Illum, excepturi? Velit
+          consectetur repudiandae iure.
+        </p>
+        <div className="btns">
+          <NavLink to="/">Return Home</NavLink>
+          <NavLink to="/contact">Report Problem</NavLink>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Error
+export default Error;
