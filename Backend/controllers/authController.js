cffd9e211795e9d2802342f5a7070afa40243ff4
Sameer <sameersattar1111@gmail.com>
2024-02-08 00:02:44 +0500

    login

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -13,11 +13,34 @@ const registerController = async (req, res) => {
 
     const userCreated = await User.create({ username, email, phone, password});
 
-    res.status(200).json({data : userCreated , token : await userCreated.generateToken() , userId : userCreated._id.toString() }); 
+    res.status(200).json({data : "Registration SuccessFully" , token : await userCreated.generateToken() , userId : userCreated._id.toString() }); 
 
   } catch (error) {  
     res.status(500).json("Internal Server Error");
   }
 };
 
-export { registerController };
+const loginController = async ( req , res) => {
+  try {
+      const {email , password} = req.body
+
+      const userExisted = await User.findOne({ email })
+
+      if (!userExisted) {
+        return res.status(400).json({ message: "Invalid Credentials" });
+      }
+
+      const isPasswordValid = await userExisted.isPasswordCorrect(password)
+ 
+      if(!isPasswordValid){
+        return res.status(401).json({ message: "Invalid email or password" });
+      }
+    
+      return res.status(200).json({data : "Login SuccessFully" , token : await userExisted.generateToken() , userId : userExisted._id.toString() }); 
+
+
+  } catch (error) {
+    res.status(500).json("Internal Server Error");
+  }
+}
+export { registerController , loginController };
--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -1,8 +1,9 @@
 import express from "express"
-import {  registerController } from "../controllers/authController.js";
+import {  loginController, registerController } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.route("/register").post(registerController)
+router.route("/login").post(loginController)
 
 export default router
